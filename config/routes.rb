4078b12f0bc03f369030c49cbf6d019224adabed
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-05-22 08:00:22 +0000

    fix:bookers2_take3_bootstrap導入まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "homes#top"
   get "top" => "homes#top"
-  get "home/about", to: "homes#about"
+  get "home/about", to: "homes#about",as: 'about'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
